d0eb96357485497a0c537a76a95e0dd6a3a46db3
Devendra Govil <govildev@gmail.com>
2023-09-21 09:59:35 -0400

    Adding test cases to test matrix dimensions for neighborhood_from_complex method

--- test/test_neighborhood.py
+++ test/test_neighborhood.py
@@ -1,6 +1,7 @@
 """Testing the neighborhood module."""
 
 import pytest
+import toponetx as tnx
 
 import topoembedx as tex
 
@@ -17,3 +18,32 @@ class TestNeighborhood:
             str(e.value)
             == """Input Complex can only be a Simplicial, Cell or Combinatorial Complex."""
         )
+
+    def test_matrix_dimensions_cellcomplex(self):
+        """Testing the matrix dimensions for the adjacency and coadjacency matrices."""
+        # Testing for the case of Cell Complex
+        cc1 = tnx.classes.CellComplex(
+            [[0, 1, 2, 3], [1, 2, 3, 4], [1, 3, 4, 5, 6, 7, 8]]
+        )
+
+        cc2 = tnx.classes.CellComplex([[0, 1, 2], [1, 2, 3]])
+
+        ind, A = tex.neighborhood.neighborhood_from_complex(cc1)
+        assert A.todense().shape == tuple([9, 9])
+        assert len(ind) == 9
+
+        ind, A = tex.neighborhood.neighborhood_from_complex(cc2)
+        assert A.todense().shape == tuple([4, 4])
+        assert len(ind) == 4
+
+        ind, A = tex.neighborhood.neighborhood_from_complex(
+            cc1, neighborhood_type="!adj"
+        )
+        assert A.todense().shape == tuple([9, 9])
+        assert len(ind) == 9
+
+        ind, A = tex.neighborhood.neighborhood_from_complex(
+            cc2, neighborhood_type="!adj"
+        )
+        assert A.todense().shape == tuple([4, 4])
+        assert len(ind) == 4
