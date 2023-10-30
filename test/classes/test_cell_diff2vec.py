69ca39e14633805960c5e8993a599cf4aecf19e2
jarpri <j.prilepok@gmail.com>
2023-10-30 14:59:46 +0000

    Formatting fix.

--- test/classes/test_cell_diff2vec.py
+++ test/classes/test_cell_diff2vec.py
@@ -9,18 +9,6 @@ from topoembedx.classes.cell_diff2vec import CellDiff2Vec
 class TestDiff2Vec:
     """Test Diff2Vec class."""
 
-    def test_fit(self):
-        """Test the fit method."""
-        # Create a small graph
-        cc_large = tnx.classes.CellComplex([[i, i + 1, i + 2, i + 3] for i in range(0, 100, 3)], ranks=2)
-
-        model = CellDiff2Vec(dimensions=2)
-        try:
-            model.fit(cc_large)
-            print("Model 3 fit successfully")
-        except Exception as e:
-            print("Error during fit for Model 3:", str(e))
-
     def test_init(self):
         """Test get_embedding."""
         # Create a small graph
@@ -29,3 +17,14 @@ class TestDiff2Vec:
 
         # Create a CellDiff2Vec object
         _ = CellDiff2Vec(dimensions=2)
+
+    def test_fit(self):
+        """Test the fit method."""
+        # Create a larger graph
+        cx_large = tnx.classes.CellComplex([[i, i + 1, i + 2, i + 3] for i in range(0, 100, 3)], ranks=2)
+
+        # Create a CellDiff2Vec object
+        model = CellDiff2Vec(dimensions=2)
+
+        # Fit the CellDiff2Vec object to the graph and get embedding for nodes
+        model.fit(cx_large)
